9fc1d62c2701eff3caccc2668fd2e8ac8004acb8
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-17 17:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let ul = document.getElentsByTagName("ul")[0];
 let text ;
 for(let i=0;i<ul.children.length;i++){
-	let id=ul.children[i];
-	if(id.id=="level"){
+	let node=ul.children[i];
+	if(node.id=="level"){
 		text = "The level of the element is: "(i+1)
 	}
 }
