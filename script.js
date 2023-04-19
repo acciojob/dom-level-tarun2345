b218015082d32126191b0cf88c480d9e1e82dce3
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-19 20:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let text ;
 for(let i=0;i<ul.children.length;i++){
 	let node=ul.children[i];
 	if(node.id === level){
-		text = "The level of the element is: "(i+1)
+		alert("The level of the element is: "(i+1));
 	}
 }
-alert(text);
